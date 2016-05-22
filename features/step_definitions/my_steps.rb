cd095301e7573fab998b6c6eef58ebb75f119308
wawrzynk <k.wawrzynkowski@yahoo.pl>
2016-05-22 18:11:27 +0200

    added steps file

---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,13 @@
+
+When(/^I open "([^"]*)"$/) do |url|
+  Page.open_url(url)
+end
+
+And(/^I search for route from "([^"]*)" to "([^"]*)"$/) do |from,to|
+  SearchPage.search_from_to(from,to)
+end
+
+Then(/^prices of the tickets are displayed and sorted ascending$/) do
+  prices_list = SearchPage.get_train_prices
+  expect(prices_list).to eq prices_list.sort
+end
