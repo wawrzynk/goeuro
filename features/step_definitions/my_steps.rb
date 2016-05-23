a66f17cd9170cdf8c1fbdfc171d794f49b32512e
wawrzynk <k.wawrzynkowski@yahoo.pl>
2016-05-23 20:12:45 +0200

    fixed test failure - incorrect sorting

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -8,7 +8,6 @@ And(/^I search for route from "([^"]*)" to "([^"]*)"$/) do |from,to|
 end
 
 Then(/^prices of the tickets are displayed and sorted ascending$/) do
-  prices_list = nil
   prices_list = SearchPage.get_train_prices
-  expect(prices_list).to eq prices_list.sort
+  expect(prices_list).to eq prices_list.sort!
 end
